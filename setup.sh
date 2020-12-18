bb268466708d5fbfb6cc9905b89d301c9c144496
gfyoung <gfyoung17+GitHub@gmail.com>
2020-12-18 21:31:59 +0000

    Trim down setup with sudo support

--- setup.sh
+++
@@ -1,8 +0,0 @@
-# Run this script after the workspace initializes.
-
-#!/usr/bin/env bash
-
-test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
